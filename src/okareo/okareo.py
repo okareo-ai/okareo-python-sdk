95b1ab204158a6ec65e917d8929a416a4dfe70f1
okasignal <boris@okareo.com>
2023-10-18 18:19:53 -0700

    generation examples using new client

--- src/okareo/okareo.py
+++ src/okareo/okareo.py
@@ -1,16 +1,24 @@
-from typing import List, Union
+import os
+from typing import Any, List, Union
 
 from okareo_api_client import Client
 from okareo_api_client.api.default import (
     create_scenario_set_v0_scenario_sets_post,
+    generate_scenario_set_v0_scenario_sets_generate_post,
     get_generations_v0_generations_get,
     register_model_v0_register_model_post,
+    scenario_sets_upload_v0_scenario_sets_upload_post,
+)
+from okareo_api_client.models.body_scenario_sets_upload_v0_scenario_sets_upload_post import (
+    BodyScenarioSetsUploadV0ScenarioSetsUploadPost,
 )
 from okareo_api_client.models.generation_list import GenerationList
 from okareo_api_client.models.http_validation_error import HTTPValidationError
 from okareo_api_client.models.model_under_test_schema import ModelUnderTestSchema
 from okareo_api_client.models.scenario_set_create import ScenarioSetCreate
+from okareo_api_client.models.scenario_set_generate import ScenarioSetGenerate
 from okareo_api_client.models.scenario_set_response import ScenarioSetResponse
+from okareo_api_client.types import UNSET, File, Unset
 
 from .common import BASE_URL, HTTPX_TIME_OUT
 from .model_under_test import ModelUnderTest
@@ -50,6 +58,7 @@ class Okareo:
         response = register_model_v0_register_model_post.sync(
             client=self.client, api_key=self.api_key, json_body=json_body
         )
+
         if isinstance(response, HTTPValidationError):
             print(f"Unexpected {response=}, {type(response)=}")
             raise
@@ -59,14 +68,66 @@ class Okareo:
 
         return ModelUnderTest(client=self.client, api_key=self.api_key, mut=response)
 
-    def create_scenario_set(self, json_body: ScenarioSetCreate) -> ScenarioSetResponse:
+    def create_scenario_set(
+        self, create_request: ScenarioSetCreate
+    ) -> ScenarioSetResponse:
         response = create_scenario_set_v0_scenario_sets_post.sync(
-            client=self.client, api_key=self.api_key, json_body=json_body
+            client=self.client, api_key=self.api_key, json_body=create_request
         )
+        # fake repetitive code
         if isinstance(response, HTTPValidationError):
             print(f"Unexpected {response=}, {type(response)=}")
             raise
         if not response:
             print("Empty response from API")
         assert response is not None
+
         return response
+
+    def upload_scenario_set(
+        self, scenario_name: str, file_path: str, project_id: Union[Unset, str] = UNSET
+    ) -> ScenarioSetResponse:
+        file_name = os.path.basename(file_path)
+
+        with open(file_path, "rb") as binary_io:
+            multipart_body = BodyScenarioSetsUploadV0ScenarioSetsUploadPost(
+                name=scenario_name,
+                project_id=project_id,
+                file=File(file_name=file_name, payload=binary_io),
+            )
+            response = scenario_sets_upload_v0_scenario_sets_upload_post.sync(
+                client=self.client, api_key=self.api_key, multipart_data=multipart_body
+            )
+
+        if isinstance(response, HTTPValidationError):
+            print(f"Unexpected {response=}, {type(response)=}")
+            raise
+        if not response:
+            print("Empty response from API")
+        assert response is not None
+
+        return response
+
+    def generate_scenario_set(
+        self, create_request: ScenarioSetGenerate
+    ) -> ScenarioSetResponse:
+        response = generate_scenario_set_v0_scenario_sets_generate_post.sync(
+            client=self.client, api_key=self.api_key, json_body=create_request
+        )
+
+        if isinstance(response, HTTPValidationError):
+            print(f"Unexpected {response=}, {type(response)=}")
+            raise
+        if not response:
+            print("Empty response from API")
+        assert response is not None
+
+        return response
+
+    def validate_response(self, response: Any) -> None:
+        if isinstance(response, HTTPValidationError):
+            print(f"Unexpected {response=}, {type(response)=}")
+            raise
+        if not response:
+            print("Empty response from API")
+        assert response is not None
