0056199ef63344961f059b107357e2b7fef25189
berni <berni@extensa.pl>
2023-11-20 17:00:07 +0100

    add run_test integration test

--- okareo_tests/test_model_under_test.py
+++ okareo_tests/test_model_under_test.py
@@ -1,23 +1,108 @@
+import random
 from datetime import datetime
+from typing import Tuple
 
 import pytest
+from okareo_tests.common import API_KEY, OkareoAPIhost, integration, random_string
 from pytest_httpx import HTTPXMock
 
 from okareo import ModelUnderTest, Okareo
-from okareo_api_client.models import TestRunItem
+from okareo_api_client.models import SeedData, TestRunItem
 from okareo_api_client.models.http_validation_error import HTTPValidationError
+from okareo_api_client.models.scenario_set_create import ScenarioSetCreate
 
 
 def helper_register_model(httpx_mock: HTTPXMock) -> ModelUnderTest:
-    fixture = {
-        "id": "1",
-        "project_id": "1",
-        "name": "NotebookModel",
-        "tags": ["ci-testing"],
-    }
+    fixture = get_mut_fixture("NotebookModel")
     httpx_mock.add_response(status_code=201, json=fixture)
     okareo = Okareo("api-key", "http://mocked.com")
     return okareo.register_model(name="NotebookModel", tags=["ci-testing"])
+
+
+def get_mut_fixture(name: str | None = None) -> dict:
+    rnd_str = random_string(5)
+    return {
+        "id": "1",
+        "project_id": "1",
+        "name": name if name else f"CI-Test-Model-{rnd_str}",
+        "tags": ["ci-testing"],
+    }
+
+
+@integration
+def test_register_model(httpx_mock: HTTPXMock, okareo_api: OkareoAPIhost) -> None:
+    fixture = get_mut_fixture()
+    if okareo_api.is_mock:
+        httpx_mock.add_response(status_code=201, json=fixture)
+
+    okareo = Okareo(api_key=API_KEY, base_path=okareo_api.path)
+    mut = okareo.register_model(name=fixture["name"], tags=fixture["tags"])
+    assert mut.name == fixture["name"]
+
+
+@integration
+def test_mut_test_run(httpx_mock: HTTPXMock, okareo_api: OkareoAPIhost) -> None:
+    mut_fixture = get_mut_fixture()
+    okareo = Okareo(api_key=API_KEY, base_path=okareo_api.path)
+
+    if okareo_api.is_mock:
+        httpx_mock.add_response(
+            status_code=201,
+            json={
+                "scenario_id": "scenario-id",
+                "project_id": "project-id",
+                "time_created": str(datetime.now()),
+                "type": "REPHRASE_INVARIANT",
+            },
+        )
+        httpx_mock.add_response(status_code=201, json=mut_fixture)
+        httpx_mock.add_response(
+            status_code=200,
+            json=[
+                {
+                    "id": "test-run-item-id",
+                    "name": "CI run test",
+                    "input": "",
+                    "result": "",
+                }
+            ],
+        )
+        # add test run
+        httpx_mock.add_response(
+            status_code=201,
+            json={
+                "id": "test-run-id",
+                "project_id": "",
+                "mut_id": "",
+                "scenario_set_id": "",
+                "name": "CI run test",
+            },
+        )
+
+    # generate scenario and return results in one call
+    scenario_set_create = ScenarioSetCreate(
+        name="my test scenario set",
+        number_examples=1,
+        seed_data=[
+            SeedData(input_="example question or statement", result="example result")
+        ],
+    )
+    response = okareo.create_scenario_set(scenario_set_create)
+    scenario_id = response.scenario_id
+
+    def call_model(input_: str) -> Tuple[str, dict]:
+        actual = random.choice(["returns", "complains", "pricing"])
+        # return a tuple of (actual, overall model response context)
+        return actual, {"labels": actual, "confidence": 0.8}
+
+    # this will return a model if it already exists or create a new one if it doesn't
+    mut = okareo.register_model(name=mut_fixture["name"], tags=mut_fixture["tags"])
+
+    # use the scenario id from one of the scenario set notebook examples
+    test_run_item = mut.run_test(
+        scenario_id=scenario_id, model_invoker=call_model, test_run_name="CI run test"
+    )
+    assert test_run_item.name == "CI run test"
 
 
 def test_add_datapoint(httpx_mock: HTTPXMock) -> None:
