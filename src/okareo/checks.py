f06f2e68ac222de690320a98f4fa90386fcf754f
Mason del Rosario <mason@okareo.com>
2024-05-08 13:52:22 -0700

    precommit

--- src/okareo/checks.py
+++ src/okareo/checks.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Any
+
 
 class BaseCheck(ABC):
     @staticmethod
     @abstractmethod
-    def evaluate(model_output: str, scenario_input: str, scenario_result: str) -> Any:
+    def evaluate(
+        model_output: str, scenario_input: str, scenario_result: str
+    ) -> bool | int | float:
         pass
