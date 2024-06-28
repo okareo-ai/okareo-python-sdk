e09ec09a3ec05007d131931e13f2aa7573710756
hrithikd <hrithikd@berkeley.edu>
2024-06-28 16:01:24 -0700

    fix precommit formatting

--- okareo_tests/sample_check.py
+++ okareo_tests/sample_check.py
@@ -4,4 +4,8 @@ from okareo.checks import BaseCheck
 class Check(BaseCheck):
     @staticmethod
     def evaluate(model_output: str, scenario_input: str, scenario_result: str) -> bool:
-        return True if len(model_output + scenario_input + scenario_result) >= 20 else False
+        return (
+            True
+            if len(model_output + scenario_input + scenario_result) >= 20
+            else False
+        )
