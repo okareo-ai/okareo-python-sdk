0bd1e8c4360cc0a140edc4f933277a572ac6a2b0
hrithikd <hrithikd@berkeley.edu>
2024-08-15 21:32:03 -0700

    update long running test

--- okareo_tests/test_integration_multiturn.py
+++ okareo_tests/test_integration_multiturn.py
@@ -278,7 +278,7 @@ def test_run_multiturn_custom_with_repeats(rnd: str, okareo: Okareo) -> None:
 
 class LongRunningMultiTurn(CustomModel):
     def invoke(self, data: Any) -> ModelInvocation:
-        time.sleep(61)
+        time.sleep(10)
         session_id = "123"
         content = "I can't help you with that."
         return ModelInvocation(content, data, {}, session_id)
