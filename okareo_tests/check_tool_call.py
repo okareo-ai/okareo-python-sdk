4e6b602db4021ae7fb40157917c8ee642cec3ec3
hrithikd <hrithikd@berkeley.edu>
2024-09-16 18:26:12 -0700

    update test check

--- okareo_tests/check_tool_call.py
+++ okareo_tests/check_tool_call.py
@@ -3,9 +3,7 @@ from okareo.checks import CodeBasedCheck
 
 class Check(CodeBasedCheck):
     @staticmethod
-    def evaluate(
-        model_output: str, scenario_input: str, scenario_result: str, metadata: dict
-    ) -> bool:
+    def evaluate(model_output: str, scenario_input: str, scenario_result: str, metadata: dict) -> bool:  # fmt: skip
         # Check if metadata is empty
         if metadata.get("tool_calls"):
             return True if len(metadata["tool_calls"]) > 0 else False
