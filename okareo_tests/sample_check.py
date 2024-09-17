ecbc09584cf557337c4d53837dd2c845b8687b25
hrithikd <hrithikd@berkeley.edu>
2024-09-16 18:15:39 -0700

    update check method

--- okareo_tests/sample_check.py
+++ okareo_tests/sample_check.py
@@ -3,9 +3,7 @@ from okareo.checks import CodeBasedCheck
 
 class Check(CodeBasedCheck):
     @staticmethod
-    def evaluate(
-        model_output: str, scenario_input: str, scenario_result: str, metadata: dict
-    ) -> bool:
+    def evaluate(model_output: str, scenario_input: str, scenario_result: str, metadata: dict) -> bool:  # fmt: skip
         return (
             True
             if len(model_output + scenario_input + scenario_result) >= 20
