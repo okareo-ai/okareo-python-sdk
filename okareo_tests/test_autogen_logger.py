d7c839988829456010f33494dcf1c1afbcf7185b
Mason del Rosario <mason@okareo.com>
2024-10-08 15:44:33 -0700

    precommit

--- okareo_tests/test_autogen_logger.py
+++ okareo_tests/test_autogen_logger.py
@@ -1,9 +1,6 @@
-import time
-from datetime import datetime
 from typing import Any
 
-import sys
-import autogen # type: ignore
+import autogen  # type: ignore
 from okareo_tests.common import API_KEY, OPENAI_API_KEY, random_string
 
 from okareo import Okareo
@@ -11,6 +8,7 @@ from okareo.autogen_logger import AutogenLogger, OkareoLogger
 from okareo_api_client.models.datapoint_search import DatapointSearch
 
 OPENAI_MODEL = "gpt-4-0125-preview"
+
 
 def get_logger_config() -> dict[str, Any]:
     context_token = random_string(10)
@@ -21,17 +19,19 @@ def get_logger_config() -> dict[str, Any]:
         "context_token": context_token,
         "log_new_agent": True,
     }
-    return logger_config 
+    return logger_config
 
 
 def create_autogen_assistant() -> Any:
     gpt4_config = {
         "cache_seed": 42,  # change the cache_seed for different trials
         "temperature": 0,
-        "config_list": [{
-            "model": OPENAI_MODEL,
-            "api_key": OPENAI_API_KEY,
-        }],
+        "config_list": [
+            {
+                "model": OPENAI_MODEL,
+                "api_key": OPENAI_API_KEY,
+            }
+        ],
         "timeout": 120,
     }
 
@@ -39,9 +39,10 @@ def create_autogen_assistant() -> Any:
         name="coder",
         llm_config=gpt4_config,
         system_message="""You are the Coder. Given a user query, You write python/shell code to solve tasks.""",
-        description="Writes code that helps answer user queries."
+        description="Writes code that helps answer user queries.",
     )
     return coder
+
 
 def test_autogen_autogenlogger() -> None:
 
@@ -52,8 +53,11 @@ def test_autogen_autogenlogger() -> None:
         create_autogen_assistant()
 
     okareo = Okareo(api_key=API_KEY)
-    dp = okareo.find_datapoints(DatapointSearch(context_token=logger_config['context_token']))
+    dp = okareo.find_datapoints(
+        DatapointSearch(context_token=logger_config["context_token"])
+    )
     assert isinstance(dp, list)
+
 
 def test_autogen_okareologger() -> None:
 
@@ -65,5 +69,7 @@ def test_autogen_okareologger() -> None:
     autogen.runtime_logging.stop()
 
     okareo = Okareo(api_key=API_KEY)
-    dp = okareo.find_datapoints(DatapointSearch(context_token=logger_config['context_token']))
+    dp = okareo.find_datapoints(
+        DatapointSearch(context_token=logger_config["context_token"])
+    )
     assert isinstance(dp, list)
