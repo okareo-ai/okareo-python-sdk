c3a5e4ff6bf204cabfbd1b53590b374dc8a598e2
hrithikd <hrithikd@berkeley.edu>
2025-06-26 13:11:09 -0400

    pre commit fix

--- okareo_tests/check_error_raise.py
+++ okareo_tests/check_error_raise.py
@@ -5,6 +5,6 @@ class Check(CodeBasedCheck):
     @staticmethod
     def evaluate(model_output: str, scenario_input: str, scenario_result: str, metadata: dict) -> bool:  # fmt: skip
         # Check if metadata is empty
-        if scenario_input != "dummyinput":            
-            raise ValueError('Check errored out.')
+        if scenario_input != "dummyinput":
+            raise ValueError("Check errored out.")
         return True
