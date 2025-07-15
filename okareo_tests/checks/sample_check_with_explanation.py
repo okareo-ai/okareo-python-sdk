8213b905afd8be229eba9b376a1878380598a9f9
Mason del Rosario <mason@okareo.com>
2025-07-14 18:02:20 -0700

    fix precommit on sample check

--- okareo_tests/checks/sample_check_with_explanation.py
+++ okareo_tests/checks/sample_check_with_explanation.py
@@ -1,4 +1,4 @@
-from okareo.checks import CodeBasedCheck, CheckResponse
+from okareo.checks import CheckResponse, CodeBasedCheck
 
 
 class Check(CodeBasedCheck):
@@ -6,14 +6,13 @@ class Check(CodeBasedCheck):
     def evaluate(model_output: str, scenario_input: str, scenario_result: str, metadata: dict, model_input: str) -> CheckResponse:  # fmt: skip
         return (
             CheckResponse(
-                score=True,
-                explanation="The combined length is at least 20 characters."
+                score=True, explanation="The combined length is at least 20 characters."
             )
             if len(model_output + scenario_input + scenario_result + str(model_input))
             + len(metadata)
             >= 20
             else CheckResponse(
                 score=False,
-                explanation="The combined length is less than 20 characters."
+                explanation="The combined length is less than 20 characters.",
             )
         )
