3be9fc4502ae460020fec70cbc454151807c3c89
Mason del Rosario <mason@okareo.com>
2025-09-25 16:09:38 -0700

    update api client with voice

--- src/okareo_api_client/models/__init__.py
+++ src/okareo_api_client/models/__init__.py
@@ -135,6 +135,8 @@ from .test_run_payload_v2_simulation_params import TestRunPayloadV2SimulationPar
 from .test_run_type import TestRunType
 from .update_test_data_point_payload import UpdateTestDataPointPayload
 from .validation_error import ValidationError
+from .voice_stream_request import VoiceStreamRequest
+from .voice_upload_request import VoiceUploadRequest
 
 __all__ = (
     "AddModelToGroupV0GroupsGroupIdModelsPostResponseAddModelToGroupV0GroupsGroupIdModelsPost",
@@ -252,4 +254,6 @@ __all__ = (
     "TestRunType",
     "UpdateTestDataPointPayload",
     "ValidationError",
+    "VoiceStreamRequest",
+    "VoiceUploadRequest",
 )
